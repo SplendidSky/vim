87dd0c804ae18de0b647206b9e71ecccadcbc99f
陈伟宸 <530415489@qq.com>
2021-02-16 15:14:48 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,15 @@
 basepath=$(pushd `dirname $0` > /dev/null; pwd; popd > /dev/null)
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
 yum install ripgrep -y
+yum install autoconf -y
+yum install automake -y
+
+wget https://tiange-usa.oss-accelerate.aliyuncs.com/ctags.zip
+unzip ctags.zip
+cd ctags
+./autogen.sh
+./configure
+make && make install
 
 if [ ! -f "/etc/yum.repos.d/vim-latest.repo" ];then
     sudo curl -L https://copr.fedorainfracloud.org/coprs/lantw44/vim-latest/repo/epel-7/lantw44-vim-latest-epel-7.repo -o /etc/yum.repos.d/vim-latest.repo
